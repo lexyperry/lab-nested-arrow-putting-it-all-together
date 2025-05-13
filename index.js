4a6ccfa640706a1b3b45cebbb030401be52caf8d
Lexy Perry <lexy.perry61022@gmail.com>
2025-05-13 19:58:42 -0400

    donezo

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
+const userInfo={
+  username: "lexyperry123",
+  password:"1234"
+}
+function createLoginTracker(userInfo) {
+  let attemptCount = 0;
 
+  return (passwordAttempt) => {
+    attemptCount++;
+
+    if (passwordAttempt === userInfo.password && attemptCount <= 3) {
+      return "Login successful";
+    } 
+    
+    if (attemptCount <= 3) {
+      return `Attempt ${attemptCount}: Login failed`;
+    } else {
+      return "Account locked due to too many failed login attempts";
+    }
+  };
+}
 
 
 module.exports = {
